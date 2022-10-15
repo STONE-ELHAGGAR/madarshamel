404c0eddbfa022367d62c8beb2fac5ed54ba5007
STONE-ELHAGGAR <08.ahmed.elhagar@gmail.com>
2022-10-15 16:11:58 +0200

    Terms page edit

--- pages/page-terms.js
+++ pages/page-terms.js
@@ -64,17 +64,17 @@ function Terms() {
                                         </div>
                                     </div>
                                     <div className="col-lg-8">
-                                        <div className="single-detail"><img className="img-responsive bdr-16" src="assets/imgs/page/blog/single/img-1.png" alt="Agon" />
+                                        <div className="single-detail"><img className="img-responsive bdr-16" src="assets/imgs/page/blog/single/img-1.png" alt="Madarshamel" />
                                             <h6 className="mt-35 mb-25" id="section-1">Limitation of Liability</h6>
-                                            <p>Under no circumstances shall AliThemes be liable for any direct, indirect, special, incidental or consequential damages, including, but not limited to, loss of data or profit, arising out of the use, or the inability to use, the materials on this site, even if AliThemes or an authorized representative has been advised of the possibility of such damages. If your use of materials from this site results in the need for servicing, repair or correction of equipment or data, you assume any costs thereof.</p>
+                                            <p>Under no circumstances shall Madarshamel be liable for any direct, indirect, special, incidental or consequential damages, including, but not limited to, loss of data or profit, arising out of the use, or the inability to use, the materials on this site, even if Madarshamel or an authorized representative has been advised of the possibility of such damages. If your use of materials from this site results in the need for servicing, repair or correction of equipment or data, you assume any costs thereof.</p>
                                             <h6 className="mt-35 mb-25" id="section-2">Licensing Policy</h6>
-                                            <p>AliThemes WordPress plugins and template are released under the GNU General Public License v2 or later. Please refer to licensing policy page for licensing details.</p>
+                                            <p>Madarshamel WordPress plugins and template are released under the GNU General Public License v2 or later. Please refer to licensing policy page for licensing details.</p>
                                             <h6 className="mt-35 mb-25" id="section-3">Product Compatibility</h6>
                                             <p>The products are developed to be compatible with the latest version of WordPress because we always strive to stay up-to-date with the latest version of WordPress. You might experience certain performance or functionality glitches with the products if you use any version prior to that.</p>
                                             <h6 className="mt-35 mb-25" id="section-4">Delivery</h6>
-                                            <p>Your purchased product(s) information will be emailed to the email address (that you will provide) once we receive your payment. Even though this usually takes a few minutes, it may also take up to 24 hours. You can contact us through our contact page if you do not receive your email after waiting for this time period. You will also have access to your purchased products from your account in AliThemes after logging in.</p>
+                                            <p>Your purchased product(s) information will be emailed to the email address (that you will provide) once we receive your payment. Even though this usually takes a few minutes, it may also take up to 24 hours. You can contact us through our contact page if you do not receive your email after waiting for this time period. You will also have access to your purchased products from your account in Madarshamel after logging in.</p>
                                             <h6 className="mt-35 mb-25" id="section-5">Ownership</h6>
-                                            <p>All the products are the property of AliThemes. So you may not claim ownership (intellectual or exclusive) over any of our products, modified or unmodified. Our products come ‘as is’, without any kind of warranty, either expressed or implied. Under no circumstances can our juridical person be accountable for any damages including, but not limited to, direct, indirect, special, incidental or consequential damages or other losses originating from the employment of or incapacity to use our products.</p>
+                                            <p>All the products are the property of Madarshamel. So you may not claim ownership (intellectual or exclusive) over any of our products, modified or unmodified. Our products come ‘as is’, without any kind of warranty, either expressed or implied. Under no circumstances can our juridical person be accountable for any damages including, but not limited to, direct, indirect, special, incidental or consequential damages or other losses originating from the employment of or incapacity to use our products.</p>
                                             <h6 className="mt-35 mb-25" id="section-6">Browser Compatibility</h6>
                                             <p>We consider it our duty to offer a great experience across most major browsers, which is why our products support the latest modern web browsers including (but not limited to) Firefox, Safari, Chrome &amp; Internet Explorer 9+. However, the performance may vary between different browsers, versions, and operating systems.</p>
                                             <h6 className="mt-35 mb-25" id="section-7">Updates</h6>
@@ -82,16 +82,16 @@ function Terms() {
                                             <h6 className="mt-35 mb-25" id="section-8">Template Support</h6>
                                             <p>Please refer to Help and Support Policy page for details.</p>
                                             <h6 className="mt-35 mb-25" id="section-9">Price Changes</h6>
-                                            <p>AliThemes reserves the right to modify or suspend (temporarily or permanently) a subscription at any point of time and from time to time with or without any notice.</p>
+                                            <p>Madarshamel reserves the right to modify or suspend (temporarily or permanently) a subscription at any point of time and from time to time with or without any notice.</p>
                                             <h6 className="mt-35 mb-25" id="section-10">Refund Policy</h6>
                                             <p>Please refer to Refund Policy page for details.</p>
                                             <h6 className="mt-35 mb-25" id="section-11">Email &amp; Newsletter</h6>
-                                            <p>AliThemes may occasionally send you emails related to the purchase of product(s) from our company. We may also send you occasional email newsletters relating to AliThemes updates and promotions. We will never sell or release your email to any third party vendors. You may opt out of these emails at any time without penalty.</p>
+                                            <p>Madarshamel may occasionally send you emails related to the purchase of product(s) from our company. We may also send you occasional email newsletters relating to Madarshamel updates and promotions. We will never sell or release your email to any third party vendors. You may opt out of these emails at any time without penalty.</p>
                                             <h6 className="mt-35 mb-25" id="section-12">License Agreement</h6>
                                             <p>By purchasing our product(s), you indicate and consent that you have read and agree to the Terms &amp; Conditions listed and detailed on this page. We reserve the right to change or modify the current Terms and Conditions without prior notice or consent.</p>
                                             <h6 className="mt-35 mb-25" id="section-13">Severability</h6>
                                             <p>If any part of this agreement is declared unenforceable or invalid, all remaining clauses in this agreement shall remain binding on the customer.</p>
-                                            <h6 className="mt-50">- AliThemes Team -</h6>
+                                            <h6 className="mt-50">- Madarshamel Team -</h6>
                                         </div>
                                     </div>
                                     <div className="col-lg-2">
@@ -121,7 +121,7 @@ function Terms() {
                                             </div>
                                         </div>
                                         <div className="col-lg-7 col-md-5 mt-30 mt-lg-0 mt-md-30 mt-sm-30 position-relative text-end">
-                                            <div className="block-chart shape-1"><img src="/assets/imgs/template/chart.png" alt="Agon" /></div><img className="img-responsive img-newsletter" src="assets/imgs/template/img-newsletter.png" alt="Agon" />
+                                            <div className="block-chart shape-1"><img src="/assets/imgs/template/chart.png" alt="Madarshamel" /></div><img className="img-responsive img-newsletter" src="assets/imgs/template/img-newsletter.png" alt="Madarshamel" />
                                         </div>
                                     </div>
                                 </div>
